075b847f7ad96ae9f46a3bc9278006abc71daf05
Snehaaa-0403 <snehaaa0403@gmail.com>
2026-04-03 23:12:43 +0530

    Update frontend API URLs for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5001/api",
+    baseURL:"https://livechat-ehwt.onrender.com/api",
     withCredentials:true //To enable to send cookies with every single request
 })
 
